ebe9bc033355f42b6cfbc5cb04680d1c820e7587
Victor Duarte <83683867+victorduarte317@users.noreply.github.com>
2022-05-18 15:44:55 -0300

    Add files via upload
    updated jwt token auth - all done

--- src/api/user/authService.js
+++ src/api/user/authService.js
@@ -21,7 +21,7 @@ const login = (req, res, next) => {
         if (err) {
             return sendErrorsFromDB(res, err)
         } else if (user && bcrypt.compareSync(password, user.password)) {
-            const token = jwt.sign(user, env.authSecret, { 
+            const token = jwt.sign({ ...user }, env.authSecret, { 
                 expiresIn: "1 day"
             })
             const { name, email } = user
@@ -39,3 +39,48 @@ const validateToken = (req, res, next) => {
         return res.status(200).send({ valid: !err })
     })
 }
+
+const signup = (req, res, any) => {
+        const name = req.body.name || ''
+        const email = req.body.email || ''
+        const password = req.body.password || ''
+        const confirmPassword = req.body.confirm_password || ''
+
+        if (!email.match(emailRegex)) {
+            return res.status(400).send({ errors: ['Invalid email.']})
+        }
+
+        if (!password.match(passwordRegex)) {
+                return res.status(400).send({errors: [
+                    "The password needs to have a capital and a minuscule character, a number, a special character(@#$%) and a size between 6-20 characters."
+                ]
+            })
+        }
+    
+
+    const salt = bcrypt.genSaltSync()
+    const passwordHash = bcrypt.hashSync(password, salt)
+    if(!bcrypt.compareSync(confirmPassword, passwordHash)) {
+        return res.status(400).send({errors: ['Passwords dont match.']})
+    }
+
+    User.findOne({ email }, (err, user) => {
+        if(err) {
+            return sendErrorsFromDB(res, err)
+        } else if (user) {
+            return res.status(400).send({ errors: ['User already registered.'] })
+        } else {
+            const newUser = new User({ name, email, password: passwordHash })
+            newUser.save(err => {
+                if(err){
+                    return sendErrorsFromDB(res, err)
+                } else {
+                    login(req, res, next)
+                }
+            })
+        }
+    })
+}
+
+module.exports = {login, signup, validateToken }
+
--- src/config/routes.js
+++ src/config/routes.js
@@ -1,13 +1,27 @@
 const express = require('express')
+const auth = require('./auth')
 
-module.exports = function(server) {
+module.exports = function (server) {
+    /*
+    * Routes protected by JWT Token
+    */
 
-    // Setting a basic URL that all routes can access
-    const router = express.Router()
-    server.use('/api', router)
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
 
-    // Billing cycle routes
+    protectedApi.use(auth)
+    
     const BillingCycle = require('../api/billingCycle/billingCycleService')
-    BillingCycle.register(router, '/billingCycles')
+    BillingCycle.register(protectedApi, '/billingCycles')
+    /*
+    * Open API (Unprotected Routes)
+    */
 
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+
+    const AuthService = require('../api/user/authService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/validateToken', AuthService.validateToken)
 }
